83123255b7813d3c61822b96a2cdaba1ecf0f903
ElenaStarikova <elena.g.starikova@gmail.com>
2022-06-20 21:00:02 +0700

    добавлены все данные нет игры

--- js/main.js
+++ js/main.js
@@ -3,17 +3,19 @@
 
 nextDay()
 
+
+
 function nextDay() {
     document.body.innerHTML = ""
     let h1 = document.createElement("h1")
     document.body.appendChild(h1)
     h1.classList.add("text-h1")
-    h1.innerHTML = "Привет"
+    h1.innerHTML = "Привет! Начинаем новый день!"
 
     let h3 = document.createElement("h3")
     document.body.appendChild(h3) 
     h3.classList.add("text-h3")
-    h3.innerHTML = "Это приложение У нас День 1"
+    h3.innerHTML = "День" + (parseFloat(localStorage.questionCount) + 1) 
 
     let button = document.createElement("button")
     document.body.appendChild(button)
@@ -42,77 +44,64 @@ function daySchedule (question) {
     p.classList.add("p-text")
     p.innerHTML = question.text
 
-    let button1 = document.createElement("button")
-    document.body.appendChild(button1)
-    button1.classList.add('button1')
-    button1.innerHTML = question.answers[0].text
-    
+    button(question.answers[0])
+    button(question.answers[1])
+    button(question.answers[2])
 
-    // при выборе правильного ответа выходит экран Молодец
-    button1.addEventListener("click", function() {
-        // строка удаляет все что было в body
-        document.body.innerHTML = ""
-        // на экране появляется надпись Молодец
-        let p = document.createElement("p")
-        document.body.appendChild(p)
-        p.innerHTML = "Молодец!"
-        p.classList.add("p-greatings")
-        localStorage.questionCount++
-        // переход на экран спорт через 3 сек
-        setTimeout(sport, 3000)     
-    })
 
-    let button2 = document.createElement("button")
-    document.body.appendChild(button2)
-    button2.classList.add('button1')
-    button2.innerHTML = question.answers[1].text
-    // при выборе неправильного ответа выходит экран Подумай еще
-    button2.addEventListener("click", function() {
-        // строка удаляет все что было в body
-        document.body.innerHTML = ""
-        // на экране появляется надпись Подумай еще
-        let p = document.createElement("p")
-        document.body.appendChild(p)
-        p.classList.add("p-greatings")
-        p.innerHTML = "Подумай еще" 
-        // вновь появляется вопрос с выбором вариантов ответа
-        
-        setTimeout(function () {
-            daySchedule(questions[localStorage.questionCount])
-        }, 3000)
-    })
 
-    let button3 = document.createElement("button")
-    document.body.appendChild(button3)
-    button3.classList.add('button1')
-    button3.innerHTML = question.answers[2].text
-    // при выборе неправильного ответа выходит экран Подумай еще
-    button3.addEventListener("click", function() {
-        // строка удаляет все что было в body
-        document.body.innerHTML = ""
-        // на экране появляется надпись Подумай еще
-        let p = document.createElement("p")
-        document.body.appendChild(p)
-        p.classList.add("p-greatings")
-        p.innerHTML = "Подумай еще"
-        // вновь появляется вопрос с выбором вариантов ответа
-        setTimeout(function () {
-            daySchedule(questions[localStorage.questionCount])
-        }, 3000)
-    })
+    function button (answer) {
+        let button1 = document.createElement("button")
+        document.body.appendChild(button1)
+        button1.classList.add('button1')
+        button1.innerHTML = answer.text
+        // button1.dataset.isCorrect = answer.isCorrect
+         // при выборе правильного ответа выходит экран Молодец
+        button1.addEventListener("click", function() {
+            if (answer.isCorrect) {
+                // строка удаляет все что было в body
+                document.body.innerHTML = ""
+                // на экране появляется надпись Молодец
+                let p = document.createElement("p")
+                document.body.appendChild(p)
+                p.innerHTML = "Молодец!"
+                p.classList.add("p-greatings")
+                localStorage.questionCount++
+                // переход на экран спорт через 3 сек
+                setTimeout(function () {
+                    sport(sportFacts[localStorage.questionCount])
+                }, 3000)  
+            }
+            else {
+                document.body.innerHTML = ""
+                // на экране появляется надпись Подумай еще
+                let p = document.createElement("p")
+                document.body.appendChild(p)
+                p.classList.add("p-greatings")
+                p.innerHTML = "Подумай еще" 
+                // вновь появляется вопрос с выбором вариантов ответа
+                
+                setTimeout(function () {
+                    daySchedule(questions[localStorage.questionCount])
+                }, 3000)
+            }
+        })
+       
+    }
 }
 
 
 
+
 // задержка 3 секунды перед переходом на следующий экран о спорте
-function sport() {
+function sport(sport) {
     // строка удаляет все что было в body
     document.body.innerHTML = ""
     // на экране появляется факт о спорте
     let p = document.createElement("p")
     document.body.appendChild(p)
     p.classList.add("p-sport")
-    p.innerHTML = "Спорт это хорошо" 
+    p.innerHTML = sport.text
     // кнопка Далее
     let button4 = document.createElement("button")
     document.body.appendChild(button4)
@@ -125,8 +114,11 @@ function sport() {
     //     // !!! на экране появляется  игра
         // })
 // похвала за пройденную игру, факт о пище, кнопка следующий день. Функция должна включится по окончанию игры
-    button4.addEventListener("click", dayChange)
-    function dayChange () {
+    function onButton () {
+        dayChange(dietFacts[localStorage.questionCount])
+    }
+    button4.addEventListener("click", onButton)
+    function dayChange (diet) {
         // строка удаляет все что было в body
         document.body.innerHTML = ""
         let h1 = document.createElement("h1")
@@ -137,7 +129,7 @@ function sport() {
         let p = document.createElement("p")
         document.body.appendChild(p) 
         p.classList.add("text-h3")
-        p.innerHTML = "Факт про полезные и вредные продукты"
+        p.innerHTML = diet.text
 
         let button5 = document.createElement("button")
         document.body.appendChild(button5)
@@ -151,7 +143,7 @@ function sport() {
             let p1 = document.createElement("p")
             document.body.appendChild(p1) 
             p1.classList.add("text-h1")
-            p1.innerHTML = "программа следующего дня будет доступна завтра"
+            p1.innerHTML = "Программа следующего дня будет доступна завтра"
         }
 
         button5.addEventListener("click", delay)
